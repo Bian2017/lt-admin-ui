29e83ec45464516371f4c7006421ac0da6285817
Bian2017 <bgl_cumt_zju@163.com>
2020-03-26 17:02:09 +0800

    feat：针对菜单栏添加样式

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <Menu defaultIndex={0} onSelect={index => alert(index)}>
+        <Menu defaultIndex={0} onSelect={index => console.log(index)} mode={'vertical'}>
           <MenuItem index={0}>cool link</MenuItem>
           <MenuItem index={1}>cool link 2</MenuItem>
           <MenuItem index={2}>cool link 3</MenuItem>
