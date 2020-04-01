127a0f91e576d95674f0c5f8664a0e02f529a6c8
Bian2017 <bgl_cumt_zju@163.com>
2020-04-01 21:55:05 +0800

    feat:添加子菜单栏相关的测试用例

--- src/components/Menu/menu.test.tsx
+++ src/components/Menu/menu.test.tsx
@@ -1,17 +1,18 @@
 import React from 'react';
-import { render, RenderResult, fireEvent, cleanup } from '@testing-library/react';
+import { render, RenderResult, fireEvent, cleanup, wait } from '@testing-library/react';
 
 import Menu, { MenuProps } from './menu';
 import MenuItem from './menuItem';
+import SubMenu from './subMenu';
 
 const testProps: MenuProps = {
-  defaultIndex: 0,
+  defaultIndex: '0',
   onSelect: jest.fn(),
   className: 'test'
 };
 
 const testVerProps: MenuProps = {
-  defaultIndex: 0,
+  defaultIndex: '0',
   mode: 'vertical'
 };
 
@@ -21,8 +22,28 @@ const generateMenu = (props: MenuProps) => {
       <MenuItem>active</MenuItem>
       <MenuItem disabled>disabled</MenuItem>
       <MenuItem>menu3</MenuItem>
+      <SubMenu title="dropdown">
+        <MenuItem>drop1</MenuItem>
+      </SubMenu>
     </Menu>
   );
+};
+
+// 创建一小部分CSS样式，避免引入整个style文件
+const createStyleFile = () => {
+  const cssFile: string = `
+    .submenu {
+      display: none;
+    }
+    .submenu.menu-opened {
+      display: block;
+    }
+  `;
+
+  const style = document.createElement('style');
+  style.type = 'text/css';
+  style.innerHTML = cssFile;
+  return style;
 };
 
 let wrapper: RenderResult, menuElement: HTMLElement, activeElement: HTMLElement, disabledElement: HTMLElement;
@@ -34,6 +55,9 @@ describe('test Menu and MenuItem component', () => {
    */
   beforeEach(() => {
     wrapper = render(generateMenu(testProps));
+
+    // 注意：wrapper.container是HTMLElement
+    wrapper.container.append(createStyleFile()); // 插入css文件
 
     // 如果要取Menu节点，可以在外层元素加上data-testid
     menuElement = wrapper.getByTestId('test-menu');
@@ -48,7 +72,10 @@ describe('test Menu and MenuItem component', () => {
     expect(menuElement).toHaveClass('menu test');
 
     // 应该有三个菜单栏节点
-    expect(menuElement.getElementsByTagName('li').length).toEqual(3);
+    // expect(menuElement.getElementsByTagName('li').length).toEqual(3); // 不分层次
+
+    // 此处 :scope表示menuElement本身，然后选择menuElement元素下的一级li元素
+    expect(menuElement.querySelectorAll(':scope > li').length).toEqual(4);
 
     expect(activeElement).toHaveClass('menu-item is-active');
     expect(disabledElement).toHaveClass('menu-item is-disabled');
@@ -63,12 +90,12 @@ describe('test Menu and MenuItem component', () => {
     expect(activeElement).not.toHaveClass('is-active');
 
     // 希望函数被调用的参数是2
-    expect(testProps.onSelect).toHaveBeenCalledWith(2);
+    expect(testProps.onSelect).toHaveBeenCalledWith('2');
 
     fireEvent.click(disabledElement);
     // disabled元素应当点击了应该没有任何反应
     expect(disabledElement).not.toHaveClass('is-active');
-    expect(testProps.onSelect).not.toHaveBeenCalledWith(1);
+    expect(testProps.onSelect).not.toHaveBeenCalledWith('1');
   });
 
   it('should render vertical mode when mode is set to vertical', () => {
@@ -86,4 +113,34 @@ describe('test Menu and MenuItem component', () => {
 
     expect(menuElement).toHaveClass('menu-vertical');
   });
+
+  it('should show dropdown items when hover on subMenu', async () => {
+    // queryByText返回的元素是HTMLElement或者null，getByText返回的元素是HTMLElement
+    // 因为元素有可能不存在，故选择queryByText方法
+    expect(wrapper.queryByText('drop1')).not.toBeVisible(); // 判断元素是否在视野中，此处需引进css样式
+
+    const dropdownElement = wrapper.getByText('dropdown');
+    fireEvent.mouseEnter(dropdownElement);
+
+    // 通过 async、await 与 wait 函数，让断言语句延时执行
+    await wait(
+      () => {
+        // 函数中的断言会重复执行，直到断言通过或者timeout报错
+        expect(wrapper.queryByText('drop1')).toBeVisible();
+      },
+      { timeout: 500 }
+    );
+
+    // 出现下列菜单栏后，此时点击子菜单
+    fireEvent.click(wrapper.getByText('drop1'));
+    expect(testProps.onSelect).toHaveBeenCalledWith('3-0');
+
+    // 鼠标离开二级菜单栏，此时元素应当消失
+    fireEvent.mouseLeave(dropdownElement);
+    await wait(() => {
+      expect(wrapper.queryByText('drop1')).not.toBeVisible();
+    });
+  });
+
+  // 待做：添加vertical下的两种测试用例，1) 点击submenu显示dropdown 2) 测试submenu默认展开下拉菜单功能
 });
