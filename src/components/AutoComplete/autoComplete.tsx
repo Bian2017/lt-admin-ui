b2c1c664934d09e6f9f635ddb5077246f7234aa4
Bian2017 <bgl_cumt_zju@163.com>
2020-04-05 21:42:21 +0800

    feat: 完成AutoComplete组件的基本框架

---
+++ src/components/AutoComplete/autoComplete.tsx
@@ -0,0 +1,59 @@
+import React, { FC, useState, ChangeEvent } from 'react';
+import Input, { InputProps } from '../Input/input';
+
+export interface AutoCompleteProps extends Omit<InputProps, 'onSelect'> {
+  fetchSuggestions: (str: string) => string[];
+  onSelect?: (item: string) => void;
+}
+
+export const AutoComplete: FC<AutoCompleteProps> = (props) => {
+  const { fetchSuggestions, onSelect, value, ...restProps } = props;
+
+  const [inputValue, setInputValue] = useState(value);
+  const [suggestions, setSuggestions] = useState<string[]>([]);
+
+  console.log('suggestions:', suggestions);
+  const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
+    const value = e.target.value.trim();
+
+    setInputValue(value);
+    if (value) {
+      const results = fetchSuggestions(value);
+      setSuggestions(results);
+    } else {
+      setSuggestions([]);
+    }
+  };
+
+  const handleSelect = (item: string) => {
+    setInputValue(item);
+    setSuggestions([]);
+    if (onSelect) {
+      onSelect(item);
+    }
+  };
+
+  // 产生下拉菜单
+  const generateDropdown = () => {
+    return (
+      <ul>
+        {suggestions.map((item, index) => {
+          return (
+            <li key={index} onClick={() => handleSelect(item)}>
+              {item}
+            </li>
+          );
+        })}
+      </ul>
+    );
+  };
+
+  return (
+    <div className="lt-auto-complete">
+      <Input value={inputValue} onChange={handleChange} {...restProps} />
+      {suggestions.length > 0 && generateDropdown()}
+    </div>
+  );
+};
+
+export default AutoComplete;
--- src/components/Input/input.tsx
+++ src/components/Input/input.tsx
@@ -66,3 +66,5 @@ export const Input: FC<InputProps> = (props) => {
     </div>
   );
 };
+
+export default Input;
