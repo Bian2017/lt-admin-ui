a0a94dd786caa488a8c3945d9d42fb2b3c344f72
Bian2017 <bgl_cumt_zju@163.com>
2020-04-18 12:29:08 +0800

    feat:解决版本升级造成的storybook编译错误问题

--- src/components/Transition/transition.tsx
+++ src/components/Transition/transition.tsx
@@ -5,12 +5,12 @@ import { CSSTransitionProps } from 'react-transition-group/CSSTransition';
 // 字符串字面量
 type AnimationName = 'zoom-in-top' | 'zoom-in-left' | 'zoom-in-bottom' | 'zoom-in-right';
 
-interface TransitionProps extends CSSTransitionProps {
+type TransitionProps = CSSTransitionProps & {
   animation?: AnimationName;
   wrapper?: boolean; // 防止子节点的transition属性覆盖动画效果，通过包裹节点来避免这一问题
-}
+};
 
-const Transition: React.FC<TransitionProps> = props => {
+const Transition: React.FC<TransitionProps> = (props) => {
   const { children, wrapper, classNames, animation, ...restProps } = props;
 
   return (
@@ -24,7 +24,7 @@ Transition.defaultProps = {
   // appear 表示如果menOpen第一次值为true的话，也会执行动画过程
   // unmoutOnExit 表示当状态达到exit-done时，被包裹的节点会被unmount，即子节点不存在。反之依然，一开始如果menuOpen是false状态，子节点是不存在的。
   unmountOnExit: true,
-  appear: true
+  appear: true,
 };
 
 export default Transition;
