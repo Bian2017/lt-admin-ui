a233c1d6074787f1433a0663b099fb8e79366261
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-04-18 04:31:40 +0000

    Deploy Bian2017/lt-admin-ui to github.com/Bian2017/lt-admin-ui.git:gh-pages

---
+++ precache-manifest.66e885e097f5b1c3bc718d4bca609b26.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "de855afa3241c0ea1a9580b8e7b3065b",
+    "url": "iframe.html"
+  },
+  {
+    "url": "main.6609fb853381adbcf37f.bundle.js"
+  },
+  {
+    "url": "runtime~main.6609fb853381adbcf37f.bundle.js"
+  },
+  {
+    "revision": "009824fd8803a870c36f",
+    "url": "static/css/main.f65d9c7e.chunk.css"
+  },
+  {
+    "revision": "5ae4a044b5ce00510cf83ca02f3512bd",
+    "url": "static/media/style.5ae4a044.css"
+  },
+  {
+    "url": "vendors~main.6609fb853381adbcf37f.bundle.js"
+  },
+  {
+    "url": "vendors~main.6609fb853381adbcf37f.bundle.js.LICENSE.txt"
+  }
+]);
